590da4c0a567e543fd615527d92412d0cf72f996
jaydg2000 <jaydg@pobox.com>
2016-04-02 13:57:44 -0700

    Fix form

--- Arkade/Animator.h
+++ Arkade/Animator.h
@@ -13,21 +13,21 @@ namespace arkade {
 		Animator(uint32_t number_of_frames, uint32_t delay_millis, bool started = true);
 		~Animator(void);
 
-		void					start();
-		void					stop(bool reset_frame = false);
-		void					reset();
-		uint32_t				animate();
-		uint32_t				current_frame();
-		void					current_frame(uint32_t frame_number);
-		bool					is_done();
+		void				start();
+		void				stop(bool reset_frame = false);
+		void				reset();
+		uint32_t			animate();
+		uint32_t			current_frame();
+		void				current_frame(uint32_t frame_number);
+		bool				is_done();
 
 	protected:
-		uint32_t				m_current_frame;
-		bool					m_started;
-		uint32_t				m_number_of_frames;
-		uint32_t				m_delay_millis;
-		Timer					m_timer;
-		bool					m_is_done;
+		uint32_t			m_current_frame;
+		bool				m_started;
+		uint32_t			m_number_of_frames;
+		uint32_t			m_delay_millis;
+		Timer				m_timer;
+		bool				m_is_done;
 
 		virtual void			on_animate() = 0;
 		virtual void			on_reset();
