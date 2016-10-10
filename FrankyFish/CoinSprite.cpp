15b13b62f20ac11041baf0b6048ee9e61d6e5f8e
Jay Green <jay.green@aa.com>
2016-10-10 13:32:19 -0700

    Tighten collision rect on coins.

--- FrankyFish/CoinSprite.cpp
+++ FrankyFish/CoinSprite.cpp
@@ -5,11 +5,11 @@
 
 
 CoinSprite::CoinSprite()
-	:RewardSprite("res/sprites/sprite_coin.png", make_size(66, 65), 10)
+	:RewardSprite("res/sprites/sprite_coin.png", make_size(66, 65), 1)
 {
 	animator(new ForwardAnimator(6, 90));
 	//animator()->current_frame(Random::rand_int(0, 5));
-	collision_dim(5,5,55,55);
+	collision_dim(20,10,25,45);
 }
 
 
